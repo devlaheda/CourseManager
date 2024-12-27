2c0f387ee56df88348b441ee2dad73255baf6b25
lahcenedaif <dev.lah.eda@gmail.com>
2024-12-27 18:17:57 +0100

    Remove Weather API

--- CourseManager.API/Program.cs
+++ CourseManager.API/Program.cs
@@ -10,12 +10,14 @@ namespace CourseManager.API
             // Add services to the container.
 
             builder.Services.AddControllers();
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
+            app.MapGet("/", () => "The App is runing");
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
